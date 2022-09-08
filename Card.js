9de27315883df098921c2717d96366a075c7e45f
Jesus Morales <110141797+Chewyy133@users.noreply.github.com>
2022-09-08 02:06:35 -0500

    Add files via upload
    Added files

---
+++ Card.js
@@ -0,0 +1,29 @@
+
+export default class Card {
+    constructor(suit, rank) {
+        this.suit = suit
+        this.rank = rank
+    }
+
+    getValue() {
+        switch (this.rank.toLowerCase()) {
+            case 'a': 
+                return 14
+            case 'k':
+                return 13
+            case 'q':
+                return 12
+            case 'j':
+                return 11
+            default:
+                return Number(this.rank)
+        }
+    }
+    getCard() {
+        return `${this.rank} of ${this.suit}`
+    }
+} 
+
+
+
+
---
+++ Player.js
@@ -0,0 +1,32 @@
+ 
+
+ class Player {
+    hand = []
+    score = 0
+    constructor(name) {
+        this.name = name
+    }
+    getPlayerName() {
+        return `${this.name}`
+    }
+    checkHand() {
+        for (const card of this.hand) {
+            console.log(card)
+        }
+        return this.hand.length
+    }
+    getCard(card) {
+        this.hand.push(card)
+    }
+    playCard() {
+        return this.hand.pop()
+    }
+    increaseScore() {
+        this.score += 1
+    }
+    getScore() {
+        return this.score
+    }
+ }
+
+
