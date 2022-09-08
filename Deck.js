4e9e0a0bad4c2f2baa8dc995c24912f6eee42ac3
Jesus Morales <jesusmorales141512@gmail.com>
2022-09-08 02:12:47 -0500

    fourth edit

--- Deck.js
+++ Deck.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import Card from './Card.js';
 
 export default class Deck {
@@ -37,44 +37,5 @@ export default class Deck {
 //newDeck.shuffleCards()
 //console.log('card dealt is:',newDeck.dealCard())
 
-=======
-import Card from './Card.js';
 
-export default class Deck {
-    cards = []
-    suits = ['clubs', 'diamonds', 'hearts', 'spades']
-    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-    constructor () {
-        for (const suit of this.suits) {
-            for (const rank of this.ranks) {
-                this.cards.push(new Card(suit, rank))
-            }
-        }
-    }
-    checkCards() {
-        for (const card of this.cards) {
-            console.log(card)
-        }
-        return this.cards.length
-    }
-    shuffleCards() {
-        for (let i = 0; i < this.cards.length; i++) {
-            const randomIndex = Math.floor(Math.random() * 52)
-            const tempCard = this.cards[i]
-            this.cards[i] = this.cards[randomIndex]
-            this.cards[randomIndex] = tempCard
-        }
-    }
-
-    dealCard() {
-        return this.cards.pop()
-    }
-}
-
-//const newDeck = new Deck();
-
-//newDeck.shuffleCards()
-//console.log('card dealt is:',newDeck.dealCard())
-
->>>>>>> 9de27315883df098921c2717d96366a075c7e45f
 //console.log(newDeck.checkCards())
--- Player.js
+++ Player.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  
 
  class Player {
@@ -31,37 +30,4 @@
  }
 
 
-=======
- 
 
- class Player {
-    hand = []
-    score = 0
-    constructor(name) {
-        this.name = name
-    }
-    getPlayerName() {
-        return `${this.name}`
-    }
-    checkHand() {
-        for (const card of this.hand) {
-            console.log(card)
-        }
-        return this.hand.length
-    }
-    getCard(card) {
-        this.hand.push(card)
-    }
-    playCard() {
-        return this.hand.pop()
-    }
-    increaseScore() {
-        this.score += 1
-    }
-    getScore() {
-        return this.score
-    }
- }
-
-
->>>>>>> 9de27315883df098921c2717d96366a075c7e45f
